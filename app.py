a04f737764d27e30f834db9ba385b5c1f7725ec1
Ubuntu <azureuser@TP1.22oycfgllxxedbahlwubpi5hng.bx.internal.cloudapp.net>
2024-04-05 09:04:31 +0000

    changement

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Welcome to the Flask CI/CD Demo! This is V7!"
+    return "Welcome to the Flask CI/CD Demo! This is V8!"
 
 @app.route("/new-deployment")
 def new_deployment():
